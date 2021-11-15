00290ac6d4d4c3fe9fb13576ec44b3e7a0b76f32
SitoNumbis <sito8943@gmail.com>
2021-11-14 21:25:15 -0500

    icon package

---
+++ src/icons/icons.jsx
@@ -0,0 +1,107 @@
+import {
+  BsFillEmojiAngryFill,
+  BsFillEmojiDizzyFill,
+  BsFillEmojiExpressionlessFill,
+  BsFillEmojiFrownFill,
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiLaughingFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiSmileUpsideDownFill,
+  BsFillEmojiSunglassesFill,
+  BsFillEmojiWinkFill,
+  BsFillExclamationCircleFill,
+  BsFillFileEarmarkArrowDownFill,
+  BsFillFileEarmarkArrowUpFill,
+  BsFillPeopleFill,
+  BsFillPersonFill,
+  BsFillPersonPlusFill,
+  BsFillPersonXFill,
+  BsFillPlayFill,
+  BsFillPlusCircleFill,
+  BsFillShareFill,
+} from "react-icons/bs";
+
+export const AngryEmojiIcon = () => {
+  return <BsFillEmojiAngryFill />;
+};
+
+export const DizzyEmojiIcon = () => {
+  return <BsFillEmojiDizzyFill />;
+};
+
+export const ExpressionlessEmojiIcon = () => {
+  return <BsFillEmojiExpressionlessFill />;
+};
+
+export const FrownEmojiIcon = () => {
+  return <BsFillEmojiFrownFill />;
+};
+
+export const HearEyesEmojiIcon = () => {
+  return <BsFillEmojiHeartEyesFill />;
+};
+
+export const LaughingEmojIcon = () => {
+  return <BsFillEmojiLaughingFill />;
+};
+
+export const NeutralEmojiIcon = () => {
+  return <BsFillEmojiNeutralFill />;
+};
+
+export const SmileEmojiIcon = () => {
+  return <BsFillEmojiSmileFill />;
+};
+
+export const SmileUpsideDownEmojiIcon = () => {
+  return <BsFillEmojiSmileUpsideDownFill />;
+};
+
+export const SunglassesEmojiIcon = () => {
+  return <BsFillEmojiSunglassesFill />;
+};
+
+export const WinkEmojiIcon = () => {
+  return <BsFillEmojiWinkFill />;
+};
+
+export const WarningIcon = () => {
+  return <BsFillExclamationCircleFill />;
+};
+
+export const DownloadIcon = () => {
+  return <BsFillFileEarmarkArrowDownFill />;
+};
+
+export const UploadIcon = () => {
+  return <BsFillFileEarmarkArrowUpFill />;
+};
+
+export const GroupIcon = () => {
+  return <BsFillPeopleFill />;
+};
+
+export const PersonIcon = () => {
+  return <BsFillPersonFill />;
+};
+
+export const AddFriendIcon = () => {
+  return <BsFillPersonPlusFill />;
+};
+
+export const DeleteFriendIcon = () => {
+  return <BsFillPersonXFill />;
+};
+
+export const SendIcon = () => {
+  return <BsFillPlayFill />;
+};
+
+export const NewChatIcon = () => {
+  return <BsFillPlusCircleFill />;
+};
+
+export const ShareIcon = () => {
+  return <BsFillShareFill />;
+};
