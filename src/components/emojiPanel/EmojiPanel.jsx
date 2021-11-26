5e343bc9d569ef23b8ecd6fc8acd7a7ab681cd21
SitoNumbis <sito8943@gmail.com>
2021-11-25 22:55:22 -0500

    emoji panel done

--- src/components/emojiPanel/EmojiPanel.jsx
+++ src/components/emojiPanel/EmojiPanel.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import * as unicodeEmoji from "unicode-emoji";
+
 import "./style.css";
 
 export let Emojis = {
@@ -16,10 +18,23 @@ export let Emojis = {
   Wink: 11,
 };
 
+const groupBy = "cateogory";
+const omitWhere = {
+  versionAbove: "12.0",
+  category: ["flags"],
+  version: ["0.6", "0.7"],
+};
+
 const EmojiPanel = (props) => {
   const [selectedEmoji, setSelectedEmoji] = useState(0);
 
-  return <div className="emoji-panel uk-background-muted"></div>;
+  return (
+    <div className="emoji-panel uk-background-muted uk-text-center" data-uk-grid>
+      {unicodeEmoji.getEmojis(omitWhere).map((d, i) => {
+        return <button className="emoji-button">{d.emoji}</button>;
+      })}
+    </div>
+  );
 };
 
 export default EmojiPanel;
