c57faba89b11bb83167293c0cbe0fbeeb2ae983b
SitoNumbis <sito8943@gmail.com>
2021-11-25 23:22:21 -0500

    working on emoji panel added emojis above 11

--- src/components/emojiPanel/EmojiPanel.jsx
+++ src/components/emojiPanel/EmojiPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import * as unicodeEmoji from "unicode-emoji";
 
@@ -18,21 +18,70 @@ export let Emojis = {
   Wink: 11,
 };
 
-const groupBy = "cateogory";
+const groupBy = "category";
 const omitWhere = {
-  versionAbove: "12.0",
+  versionAbove: "11.0",
   category: ["flags"],
   version: ["0.6", "0.7"],
 };
 
 const EmojiPanel = (props) => {
   const [selectedEmoji, setSelectedEmoji] = useState(0);
+  const [setOfEmojis, setSetOfEmojis] = useState([]);
+  const [setOfCategories, setSetOfCategories] = useState([]);
+
+  useEffect(() => {
+    const allEmojis = unicodeEmoji.getEmojisGroupedBy(groupBy, omitWhere);
+    const allCategories = Object.keys(allEmojis);
+    const newSetOfEmojis = [];
+    const newSetOfCategories = [];
+    allCategories.forEach((item) => {
+      const arrayOfEmojis = {
+        key: item,
+        emojis: allEmojis[item],
+      };
+      const category = {
+        key: item,
+        emoji: allEmojis[item][0],
+      };
+      newSetOfEmojis.push(arrayOfEmojis);
+      newSetOfCategories.push(category);
+    });
+    setSetOfCategories(newSetOfCategories);
+    setSetOfEmojis(newSetOfEmojis);
+  }, []);
 
   return (
-    <div className="emoji-panel uk-background-muted uk-text-center" data-uk-grid>
-      {unicodeEmoji.getEmojis(omitWhere).map((d, i) => {
-        return <button className="emoji-button">{d.emoji}</button>;
-      })}
+    <div className="emoji-panel uk-background-muted">
+      <ul
+        className="uk-subnav uk-subnav-pill"
+        data-uk-switcher="animation: uk-animation-slide-left-medium, uk-animation-slide-right-medium"
+      >
+        {setOfCategories.map((d, i) => {
+          return (
+            <li key={i}>
+              <a href="#">{d.emoji.emoji}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="uk-switcher uk-margin">
+        {setOfEmojis.map((d, i) => {
+          return (
+            <li key={i}>
+              <div className="uk-text-left" data-uk-grid>
+                {d.emojis.map((e, j) => {
+                  return (
+                    <button key={j} className="emoji-button">
+                      {e.emoji}
+                    </button>
+                  );
+                })}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
